34d4a62b5db99098a99b2e210beb8c49c1cb9789
Tomasz Wojewodka <tomasz.wojewodka@gmail.com>
2015-10-24 22:16:53 +0200

    Initial commit

---
+++ src/main/java/com/hudela/genealogy/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package com.hudela.genealogy;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
